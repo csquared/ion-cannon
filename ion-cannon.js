f9c15c544491005ad05b67a7a2db5992449cf201
Chris Continanza <christopher.continanza@gmail.com>
2013-06-30 19:10:07 -0700

    clustering finally working

--- ion-cannon.js
+++ ion-cannon.js
@@ -1,16 +1,49 @@
 var logfmt = require('logfmt'),
     url = require('url'),
-    argv = require('optimist').argv;
+    cluster = require('cluster'),
+    logfmt = require('logfmt'),
+    argv = require('optimist').argv,
+    numCPUs = require('os').cpus().length;
 
 exports.fire = function(){
   var target_url = url.parse(argv._[0]);
 
+  var fire_func = null;
   if(target_url.protocol == 'mqtt:'){
-    fire_mqtt(target_url, argv);
+    fire_func = function(name) { fire_mqtt(name, target_url, argv) };
+  }
+
+  var concurrency = parseInt(argv.c || 1);
+  var c_per_cpu   = concurrency / numCPUs;
+
+  if(cluster.isMaster){
+    logfmt.log({concurrency: concurrency, c_per_cpu:
+                c_per_cpu,
+                num_cpus: numCPUs})
+    for (var i = 0; i < numCPUs && i < concurrency; i++) {
+      cluster.fork();
+    }
+    //get an array of the workers
+    var workers = [];
+    for(var id in cluster.workers) {
+      workers.push(cluster.workers[id])
+    }
+    //loop over the array to send work
+    for(var i = 0; i < concurrency; i++){
+      workers[i%numCPUs].send(i);
+    }
+    cluster.on('exit', function(worker, code, signal) {
+      console.log('worker ' + worker.process.pid + ' died');
+    });
+  }else{
+    process.on('message', function(i){
+      logfmt.log({worker: cluster.worker.id, i: i})
+      fire_func('w' + cluster.worker.id + ':t' + i);
+    })
   }
 }
 
-var fire_mqtt = function(url, options){
+var fire_mqtt = function(id, url, options){
   var mqtt = require('mqtt');
   var auth = (url.auth || ':').split(':');
   // Create a client connection
@@ -22,8 +55,9 @@ var fire_mqtt = function(url, options){
   var count = 0;
 
   var chirp = function(){
-    var message = 'msg ' + count;
+    var message = 'hello from ' + id + ':' + count;
     logfmt.log({
+      id: id,
       mqtt: true,
       i: count,
       topic: 'messages',
